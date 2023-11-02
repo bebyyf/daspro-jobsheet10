9e55297422a37d2b26ec4b599151b344bcaae0a5
bebyyf <bebifindia@gmail.com>
2023-11-02 14:04:21 +0700

    MODIFIKASI PRAKTIKUM DASAR PEMOGRAMAN JOBSHEET 10 MY BABBY FINDIA R.S (21)

--- BioskopWithScanner21.java
+++ BioskopWithScanner21.java
@@ -7,16 +7,59 @@ public class BioskopWithScanner21 {
 
         String [][] penonton = new String [4][2];
         while (true) {
-            System.out.println("Masukkan nama  : ");
-            nama = bebi21.nextLine();
-            System.out.println("Masukkan baris : ");
-            baris = bebi21.nextInt();
-            System.out.println("Masukkan kolom :");
-            kolom = bebi21.nextInt();
+            System.out.println("Menu :");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.println("Pilih menu (1/2/3) : ");
+            int menu = bebi21.nextInt();
             bebi21.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
-            System.out.print("Input penonton lainnya? (y/n): ");
+            switch (menu) {
+                case 1:
+                System.out.println("=============INPUT DATA PENONTON============");
+                System.out.println("Masukkan nama  : ");
+                nama = bebi21.nextLine();
+                System.out.println("Masukkan baris : ");
+                baris = bebi21.nextInt();
+                System.out.println("Masukkan kolom :");
+                kolom = bebi21.nextInt();
+                bebi21.nextLine();
+
+                if (baris >=1 && baris <=4 &&kolom >= 1 && kolom<=2){
+                    if(penonton[baris- 1][kolom -1] == null){
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.println("Data berhasil disimpan");
+                }else{
+                    System.out.println("Kursi telah terisi, silahkan coba lagi");
+                }}else{
+                    System.out.println("Baris atau kolom tidak tersedia");
+
+                }
+                break;
+
+                case 2:
+                System.out.println("=============DAFTAR PENONTON============");
+                for (int i=0; i < penonton.length; i++){
+                    for (int j = 0; j<penonton[i].length; j++){
+                        if (penonton[i][j] !=null){
+                            System.out.println("Baris " + (i+1) + ", Kolom " + (j+1) + ":" +penonton[i][j]);
+                        }else{
+                            penonton[i][j] = "***";
+                            System.out.println("Baris " + (i+1) + ", Kolom " + (j+1) + ":" + penonton[i][j]);
+                        }
+                    }
+                }
+                break;
+
+                case 3:
+                System.out.println("Pemesanan Tersimpan, Terimakasih atas pemesanannya");
+                System.exit(0);
+                default:
+                System.out.println("Program error, Silahkan coba lagi");
+            }
+
+            System.out.print("Pilih menu lainnya? (y/n): ");
             next = bebi21.nextLine();
 
             if(next.equalsIgnoreCase("n")){
